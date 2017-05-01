4fd0ae6502dd7f81f9accfdbcbe36a5a077647e8
Brian <mayrose.dg@gmail.com>
2017-04-30 23:10:38 -0700

    first commit

---
+++ run.py
@@ -0,0 +1,15 @@
+# run.py
+
+#################
+#### imports ####
+#################
+from project.recipes import views
+from project.users import views
+from project import app
+from flask import Flask
+
+
+
+
+if __name__ == "__main__":
+   app.run()
